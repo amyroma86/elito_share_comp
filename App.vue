8f482f9da38c815b685d01b8cb02ddc50fcfcc9d
amyroma86 <158284486+amyroma86@users.noreply.github.com>
2024-04-09 00:05:52 +0200

    form contatti completo

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Contact />
+</template>
+
+<script>
+import Contact from './components/Contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contact
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: black;
+  color: white;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
